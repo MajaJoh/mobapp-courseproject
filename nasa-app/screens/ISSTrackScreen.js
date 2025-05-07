74d3c20416123adaf09a6d6622e30a34f05bf63d
Cecilia Stude <cecilia.stude@ha.ax>
2025-05-07 15:11:47 +0300

    Added ISS Tracking with React native maps to ISSTrackingScreen

--- nasa-app/screens/ISSTrackScreen.js
+++ nasa-app/screens/ISSTrackScreen.js
@@ -1,24 +1,82 @@
-import React from "react";
-import { View, Text, Button, Image, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import MapView, { Marker } from 'react-native-maps';
 
-export default function ISSTrackScreen({ navigation }) {
+export default function ISSTrackScreen() {
+  const [issData, setIssData] = useState(null); 
+  const [loading, setLoading] = useState(true);
+  const [region, setRegion] = useState({
+    latitude: 0,
+    longitude: 0,
+    latitudeDelta: 10,
+    longitudeDelta: 10,
+  });
+
+  useEffect(() => {
+    fetch("http://api.open-notify.org/iss-now.json")
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.iss_position) {
+          const { latitude, longitude } = data.iss_position;
+          setIssData(data);
+          setRegion({
+            latitude: parseFloat(latitude),
+            longitude: parseFloat(longitude),
+            latitudeDelta: 10,
+            longitudeDelta: 10,
+          });
+        }
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching ISS Data:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={styles.center}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
+  if (!issData) {
+    return (
+      <View style={styles.center}>
+        <Text>Error loading data.</Text>
+      </View>
+    );
+  }
+
+  const { latitude, longitude } = issData.iss_position;
+
   return (
-    <View style={styles.container}>
-      <Text>Test</Text>
+    <View style={styles.center}>
+      <MapView
+        style={styles.map}
+        region={region}  
+      >
+        <Marker
+          coordinate={{
+            latitude: parseFloat(latitude),
+            longitude: parseFloat(longitude),
+          }}
+          title={"ISS Location"}
+          description={`Latitude: ${latitude}, Longitude: ${longitude}`}
+        />
+      </MapView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  center: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    paddingBottom: 50,
   },
-  logo: {
-    width: 155,
-    height: 155,
-    marginBottom: 40,
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
