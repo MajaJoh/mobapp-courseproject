55addbb748eb38e4fcb9217b5609ea0ce996bf50
MajaJoh <maja.johansson@ha.ax>
2025-05-14 18:57:38 +0300

    Near earth objects done

--- nasa-app/screens/HomeScreen.js
+++ nasa-app/screens/HomeScreen.js
@@ -3,13 +3,45 @@ import { View, Button, Image, StyleSheet } from 'react-native';
 
 export default function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Image
-        source={require("../assets/nasalogo.png")}
-        style={styles.logo}
-      />
-      <Button title="Astronomy Picture of The Day" onPress={() => navigation.navigate('APOD')} />
-    </View>
+    <ScrollView>
+      <ImageBackground
+        source={require("../assets/homeImg.jpg")}
+        style={styles.backGroundImage}
+        resizeMode="cover"
+      >
+        <Image source={require("../assets/nasalogo.png")} style={styles.logo} />
+        <Text style={styles.introText}>
+          "Hello and welcome to our NASA App! Text about the app comes here :D
+        </Text>
+        <Pressable
+          style={styles.button}
+          onPress={() => navigation.navigate("APOD")}
+        >
+          <Text style={styles.buttonText}>Astronomy Picture of The Day</Text>
+        </Pressable>
+
+        <Pressable
+          style={styles.button}
+          onPress={() => navigation.navigate("ISS Tracking")}
+        >
+          <Text style={styles.buttonText}>ISS Tracking</Text>
+        </Pressable>
+
+        <Pressable
+          style={styles.button}
+          onPress={() => navigation.navigate("Rover Photos")}
+        >
+          <Text style={styles.buttonText}>Mars Rover Photos</Text>
+        </Pressable>
+
+        <Pressable
+          style={styles.button}
+          onPress={() => navigation.navigate("NEO")}
+        >
+          <Text style={styles.buttonText}>Near Earth Objects</Text>
+        </Pressable>
+      </ImageBackground>
+    </ScrollView>
   );
 }
 
